a58a2ac93f2b895192dc4fd9d1ca5f0498241fb7
tuningmind <c9@tuningmind.net>
2016-03-05 20:04:41 +0000

    Sync with c9 workspace

--- javascript/bundle.js
+++ javascript/bundle.js
@@ -44,18 +44,97 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var main = __webpack_require__(1);
+	__webpack_require__(1);
+	__webpack_require__(1);
+	module.exports = __webpack_require__(3);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var main = __webpack_require__(2);
 
 	main();
 
 /***/ },
-/* 1 */
+/* 2 */
 /***/ function(module, exports) {
 
 	module.exports = function () {
-	  document.getElementById('content').innerHTML = "got it to show up in the browser";
+	  document.getElementById('content').innerHTML = "shows up in the browser";
 	  console.log("show this in console");
 	};
+
+/***/ },
+/* 3 */
+/***/ function(module, exports) {
+
+	/******/(function (modules) {
+		// webpackBootstrap
+		/******/ // The module cache
+		/******/var installedModules = {};
+
+		/******/ // The require function
+		/******/function __webpack_require__(moduleId) {
+
+			/******/ // Check if module is in cache
+			/******/if (installedModules[moduleId])
+				/******/return installedModules[moduleId].exports;
+
+			/******/ // Create a new module (and put it into the cache)
+			/******/var module = installedModules[moduleId] = {
+				/******/exports: {},
+				/******/id: moduleId,
+				/******/loaded: false
+				/******/ };
+
+			/******/ // Execute the module function
+			/******/modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+			/******/ // Flag the module as loaded
+			/******/module.loaded = true;
+
+			/******/ // Return the exports of the module
+			/******/return module.exports;
+			/******/
+		}
+
+		/******/ // expose the modules object (__webpack_modules__)
+		/******/__webpack_require__.m = modules;
+
+		/******/ // expose the module cache
+		/******/__webpack_require__.c = installedModules;
+
+		/******/ // __webpack_public_path__
+		/******/__webpack_require__.p = "";
+
+		/******/ // Load entry module and return exports
+		/******/return __webpack_require__(0);
+		/******/
+	})(
+	/************************************************************************/
+	/******/[
+	/* 0 */
+	/***/function (module, exports, __webpack_require__) {
+
+		var main = __webpack_require__(1);
+
+		main();
+
+		/***/
+	},
+	/* 1 */
+	/***/function (module, exports) {
+
+		module.exports = function () {
+			document.getElementById('content').innerHTML = "got it to show up in the browser";
+			console.log("show this in console");
+		};
+
+		/***/
+	}
+	/******/]);
 
 /***/ }
 /******/ ]);
