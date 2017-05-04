33935e309c03fc7594e779594ada233de0c68a6f
tuningmind <tuningmind@gmail.com>
2017-05-04 17:29:36 -0600

    Scss

--- src/App.js
+++ src/App.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react'
 import './App.css';
-import CardOne from './components/CardOne.js'
-import CardTwo from './components/CardTwo.js'
-import CardThree from './components/CardThree.js'
-import CardFour from './components/CardFour.js'
 
 class App extends Component {
   render() {
     return (
-      <div id="App">
-        <header>
+      <body className="column-container">
+        <header className="column-item-top column-container">
           <h1>Carpe Diem</h1>
+          <a href="#">Instructions</a>
+          <p>How much can you get from <span className="italic">your</span> day?</p>
         </header>
-        <main>
-          <section id="gameplay">
+        <main className="column-item-center">
+          <section className="row-container" id="gameplay">
+              <div className="column-container" id="display">
+                <p className="column-item">energy</p>
+                <p className="column-item">dollars</p>
+                <p className="column-item">time</p>
+                <p className="column-item">days</p>
+              </div>
+              <div className="row-container" id="hand">
+                <div className="card" id="card1">card</div>
+                <div className="card" id="card2">card</div>
+                <div className="card" id="card3">card</div>
+                <div className="card" id="card4">card</div>
+              </div>
           </section>
           <section id="instructions">
           </section>
         </main>
-        <footer>
+        <footer className="column-item-bottom">
         </footer>
-      </div>
-    );
+      </body>
+    )
   }
 }
 
