f1cf61fc729a8eff7cc25a65537a289f0f043c48
tuningmind <tuningmind@gmail.com>
2017-05-05 15:25:03 -0600

    Place flex properties on individual elementsw

--- src/App.js
+++ src/App.js
@@ -4,36 +4,34 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="column-container">
-        <header className="column-item-top column-container">
+      <div id="myapp">
+        <header>
           <h1>Carpe Diem</h1>
           <a href="#">Instructions</a>
           <p>How much can you get from <span className="italic">your</span> day?</p>
         </header>
-        <main className="column-item-center">
-          <section className="row-container" id="gameplay">
-              <div className="column-container" id="display">
-                <p className="column-item">energy</p>
-                <p className="column-item">dollars</p>
-                <p className="column-item">time</p>
-                <p className="column-item">days</p>
+        <main>
+            <div id="gameplay">
+              <div id="display">
+                <p>energy</p>
+                <p>dollars</p>
+                <p>time</p>
+                <p>days</p>
               </div>
-              <div className="row-container" id="hand">
+              <div id="hand">
                 <div className="card" id="card1"></div>
                 <div className="card" id="card2"></div>
                 <div className="card" id="card3"></div>
                 <div className="card" id="card4"></div>
               </div>
-          </section>
-          <section id="instructions">
-          </section>
+            </div>
         </main>
-        <footer className="column-item-bottom" id="footer">
-          <div className="row-container">
-            <p className="row-item">links</p>
-            <p className="row-item">links</p>
-            <p className="row-item">links</p>
-            <p className="row-item">links</p>
+        <footer id="footer">
+          <div>
+            <p>links</p>
+            <p>links</p>
+            <p>links</p>
+            <p>links</p>
           </div>
         </footer>
       </div>
