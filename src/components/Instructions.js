a22d864de1305ffdbc47a51ae87d60753d3ba82c
tuningmind <tuningmind@gmail.com>
2017-05-06 21:50:12 -0600

    Begin setup for instructions contents

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react'
 class Instructions extends Component {
 
   render () {
+    const energy = 'Hearts: +1 energy points'
+    const dollars = 'Numbered cards get their number in dollar points'
+    const victory = 'Face cards get victory points: Ace gets 10, Jack gets 20'
+    const time = 'You get 5 time units each day. Card 1 consumes 1 time unit to play. Card 2 consumes 2 time units, Card 3 consumes 3, Card 4 consumes 4. That leaves you one left over each day, and you may freelance (trade time for money) or recuperate (trade time for energy)'
+    const days = 'You have thirteen days to play. Each day consumes 1 energy and 1 dollar in addition to your other choices.'
+
     const instructions = ["energy", "dollars", "victory points", "time", "days" ]
     return (
       <div id="instr">
