182513fbcd04a9d964a2d8dc267b8c97f7ff12c9
tuningmind <tuningmind@gmail.com>
2017-05-14 12:02:46 -0600

    Use one element for instruction name and instruction

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -8,33 +8,42 @@ class Instructions extends Component {
     const victory = 'Face cards get victory points: Ace gives 50, Jack gives 10, Queen gives 20, King gives 30. Victory points are changed directly to dollars at the end of the game.'
     const time = 'You get 5 time units each day. Card 1 consumes 1 time unit to play. Card 2 consumes 2 time units, Card 3 consumes 3, Card 4 consumes 4. That leaves you one left over each day, and you may freelance (trade time for money) or recuperate (trade time for energy)'
     const days = 'You have thirteen days to play. Each day consumes 1 energy and 1 dollar in addition to your other choices.'
-    const categories = ["energy", "dollars", "victory points","time", "days" ]
-    const instructions = [energy, dollars, victory, time, days]
-    const categoryDivs = 
-          categories.map((category, i) => 
-            ( 
-              <div key={i} className="category">
-                {category}
-              </div>
-            )
-          ) 
-    const instructionDivs = 
-          instructions.map((instruction, i) =>
-            (
-              <div key={i} className="instruction">
-                {instruction}
-              </div>
-            )
-          )
-
+    const instructionElement = [
+      {
+        name: "energy",
+        inst: energy
+      },
+      {
+        name: "dollars",
+        inst: dollars
+      },
+      {
+        name: "victory points",
+        inst: victory
+      },
+      {
+        name: "time",
+        inst: time
+      },
+      {
+        name: "days",
+        inst: days
+      }
+    ].map((element, i) =>
+      (
+        <div key={i} className="instelem">
+          <div key={element.name} className="category">
+            {element.name}
+          </div>
+          <div key={element.instruction} className="instruction">
+            {element.inst}
+          </div>
+        </div>
+      )
+    )
     return (
       <div id="instructions">
-        <div className='categoryDiv'>
-          {categoryDivs}
-        </div>
-        <div className='instructionDiv'>
-          {instructionDivs}
-        </div>
+        {instructionElement}
       </div>
     )
   }
