e662ccbe90925a0be625affa067bbc6a9d2ff325
tuningmind <tuningmind@gmail.com>
2017-05-16 10:52:06 -0600

    Decrement unused array as index is used

--- src/App.js
+++ src/App.js
@@ -12,34 +12,45 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      used: []
+      unused: [ 
+                0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 
+               10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+               20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+               30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
+               40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+               50, 51
+              ]
     }
   }
 
-  getRandomNum(used) {
+  getRandomNum(unused) {
     let randomize= (min, max) => {
       // getRandomIntInclusive
       min = Math.ceil(min);
       max = Math.floor(max);
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    let number = randomize(1, 51)
-    let cardInUsed = used.indexOf(number) > -1
-    while (cardInUsed) { number = randomize(1, 51) }
+    let number = randomize(0, 51)
+    let cardUsed = unused.indexOf(number) === -1
+    if (cardUsed) { number = randomize(0, 51) }
+    if (cardUsed) { number = randomize(0, 51) }
     return number 
   }
 
   makeHand() {
     let random = this.getRandomNum
-    let hand = [1,2,3,4].map((num) => (Cards[random(this.state.used)]))
+    let hand = [1,2,3,4].map((num) => (Cards[random(this.state.unused)]))
     return hand 
   }
 
   setUsed(four) {
-    const used = this.state.used
-    four.map((num) => used.push(num))
-    this.setState({used: used})
-    console.log("used: ", used)
+    const unused = this.state.unused
+    four.map((num) => {
+      let toremove = unused.indexOf(num)
+      return unused.splice(toremove, 1)
+    })
+    this.setState({unused: unused})
+    console.log("unused: ", unused)
   }
 
   render() {
