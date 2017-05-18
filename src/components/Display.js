2e6e733686dec7780382126106064d2c85b39bb3
tuningmind <tuningmind@gmail.com>
2017-05-17 20:23:43 -0600

    Display shows points

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,13 +3,12 @@ import React, { Component } from 'react'
 class Display extends Component {
 
   render () {
-    console.log("this.energy inside display: ", this.energy)
     return (
       <div id="display">
-        <p id="energy">{this.energy} energy</p>
-        <p id="dollars">{this.dollars} dollars</p>
-        <p id="time">{this.time} time</p>
-        <p id="days">{this.day} days</p>
+        <p id="energy">{this.props.energy} energy</p>
+        <p id="dollars">{this.props.dollars} dollars</p>
+        <p id="time">{this.props.time} time</p>
+        <p id="days">{this.props.day} days</p>
       </div>
     )
   }
